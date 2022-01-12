b2023e75baf7432b97d28060fd7f789956bdf9ff
Kishiman <fares.manaisademx@gmail.com>
2022-01-12 23:44:30 +0100

    pyFile

--- Source/KishiVisualNovel/Public/DataAssets/PyFile.h
+++ Source/KishiVisualNovel/Public/DataAssets/PyFile.h
@@ -4,21 +4,34 @@
 
 #include "CoreMinimal.h"
 #include "KishiDataAsset.h"
+#include "KishiVisualNovelGameMode.h"
+#include "Libraries/Tree/ArrayTree.h"
 #include "PyFile.generated.h"
 
 /**
- * 
  */
 USTRUCT(BlueprintType)
 struct FPyLine
 {
 	GENERATED_USTRUCT_BODY()
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Py")
-	uint8 tabs=0;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Py")
-	int LineNumber=0;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Py")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Py")
+	uint8 tabs = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Py")
+	int LineNumber = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Py")
 	FString line;
+};
+
+class PyInstruction : public IArrayTree
+{
+public:
+	FPyLine *pyLine;
+	virtual void Execute(AKishiVisualNovelGameMode *gameMode) = 0;
+
+protected:
+	PyInstruction(FPyLine *_pyLine, PyInstruction *_parent) : pyLine(_pyLine), parent(_parent){};
+	PyInstruction *parent;
+	TArray<PyInstruction *> children;
 };
 
 UCLASS()
@@ -27,6 +40,9 @@ class KISHIVISUALNOVEL_API UPyFile : public UKishiDataAsset
 	GENERATED_BODY()
 public:
 	UPROPERTY(EditAnywhere)
-	TArray<FPyLine>	PyLines;
-	static TArray<FPyLine> PYLinesFromString(FString text,uint8 TabSize);
+	TArray<FPyLine> PyLines;
+	static TArray<FPyLine> PYLinesFromString(FString text, uint8 TabSize);
+
+private:
+	TArray<PyInstruction *> instructions;
 };
