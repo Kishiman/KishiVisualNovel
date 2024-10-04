f412674fec847eb5a614787af2827e4ee66c7a9f
Kishiman <fares.manaisademx@gmail.com>
2024-10-04 01:00:04 +0100

    improve scene parsing

--- Source/KishiVisualNovel/Public/Rpy/RpyParser.h
+++ Source/KishiVisualNovel/Public/Rpy/RpyParser.h
@@ -1,11 +1,19 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "EngineUtils.h"
 #include <regex>
 #include <string>
 
 #include "Rpy/RpyScript.h"
 
+TMap<FString, FVector> const stringToFVector = {
+		{"left", FVector(0, 0.5, 0)},
+		{"right", FVector(1, 0.5, 0)},
+		{"center", FVector(0.5, 0.5, 0)},
+		{"top", FVector(0.5, 1.0, 0)},
+		{"bottom", FVector(0.5, 0, 0)},
+};
 struct RpyParser
 {
 	int paramsNum;
@@ -28,6 +36,7 @@ struct RpyParser
 	static std::string reg_float;
 	static std::string reg_ufloat;
 	static std::string reg_ufloatUnit;
+	static std::string reg_vector;
 	static std::string reg_bool;
 	static std::string reg_string;
 	static std::string reg_string_simple;
@@ -37,6 +46,7 @@ struct RpyParser
 	static std::string reg_var_name;
 	static std::string reg_args_map;
 	static std::string reg_rpy_options;
+	static std::string reg_rpy_scene_options;
 
 	static std::string reg_name;
 	static std::string reg_multi_name;
@@ -52,6 +62,29 @@ struct RpyParser
 	{
 		float value = FCString::Atof(*param);
 		return value;
+	}
+	static FVector GetVector(FString param)
+	{
+		FVector vector;
+		TArray<FString> strings;
+		static const TCHAR *delimiters[] =
+				{
+						TEXT(","),
+				};
+		param.TrimStartAndEnd().ParseIntoArray(strings, delimiters, 1, true);
+		if (strings.Num() > 0)
+		{
+			vector.X = GetFloat(strings[0]);
+		}
+		if (strings.Num() > 1)
+		{
+			vector.Y = GetFloat(strings[1]);
+		}
+		if (strings.Num() > 2)
+		{
+			vector.Z = GetFloat(strings[2]);
+		}
+		return vector;
 	}
 	static bool GetBool(FString param)
 	{
@@ -142,12 +175,49 @@ struct RpyParser
 		}
 		return options;
 	}
+	static FRpySceneOptions GetRpySceneOptions(TArray<FString> params, int offset = 0)
+	{
+		FRpySceneOptions options;
+		if ((params.Num() + offset) < 5)
+		{
+			return options;
+		}
+		FString with = params[0 + offset];
+		FString transitionTime = params[1 + offset];
+		FString positionEnum = params[2 + offset];
+		FString positionVector = params[3 + offset];
+		FString layer = params[4 + offset];
+		if (!with.IsEmpty())
+		{
+			options.with = stringToERPYTransition[with];
+			options.transitionTime = GetFloat(transitionTime);
+		}
+		if (!layer.IsEmpty())
+		{
+			options.layer = stringToELayer[layer];
+		}
+		if (!positionEnum.IsEmpty())
+		{
+			options.position = stringToFVector[positionEnum];
+		}
+		else if (!positionVector.IsEmpty())
+		{
+			auto position = GetVector(positionVector);
+			options.position.Set(position.X, position.Y, position.Z);
+		}
+		return options;
+	}
 };
+
+std::string reg_float_nc = "(?:[+-]?(?:\\d*\\.)?\\d+)";
+
 std::string RpyParser::reg_bool = "(True|False|None)";
 std::string RpyParser::reg_integer = "(\\d*)";
 std::string RpyParser::reg_float = "([+-]?(?:\\d*\\.)?\\d+)";
 std::string RpyParser::reg_ufloat = "((?:\\d*\\.)?\\d+)";
 std::string RpyParser::reg_ufloatUnit = "(0(?:\\.\\d+)?|1(?:\\.0+)?)";
+std::string RpyParser::reg_vector = "\\((\\s*" + reg_float_nc + "\\s*(?:,\\s*" + reg_float_nc + "\\s*)*)\\)";
+
 /*
 match a string containing a series of comma-separated key-value pairs, where each key is a word and each value is a string enclosed in double quotes
 		, color="#c8ffc8", voice="ok"
@@ -160,6 +230,26 @@ std::string RpyParser::reg_args_map = "((?:, \\w+=\".*\")+)";
 INCLUDE space left
 */
 std::string RpyParser::reg_rpy_options = "((?: (?:at|with|onlayer) \\w+)*)";
+
+// 'master' | 'transient' | 'underlay' | 'overlay'
+std::string reg_layer_nc = "master|transient|underlay|overlay";
+// 'left' | 'right' | 'center' | 'top' | 'bottom' | Vector2D;
+std::string reg_position_enum_nc = "left|right|center|top|bottom";
+// | 'dissolve'
+// | 'fade'
+// | 'wipeleft'
+// | 'wiperight'
+// | 'wipeup'
+// | 'wipedown'
+// | 'zoom'
+// | 'blur'
+std::string reg_transition_enum_nc = "dissolve|fade|wipeleft|wiperight|wipeup|wipedown|zoom|blur";
+
+// 2
+std::string reg_position = "(?:(" + reg_position_enum_nc + ")|" + RpyParser::reg_vector + ")";
+
+std::string RpyParser::reg_rpy_scene_options = "(?:(?:\\s*with\\s*(" + reg_transition_enum_nc + ")\\s*(" + reg_float_nc + ")?)?(?:\\s*at\\s*" + reg_position + ")?(?:\\s*onlayer\\s*(" + reg_layer_nc + "))?)";
+
 /*
 (?:\"(?:[^\"\\\\]|\\\\.)+\")
 (?:'(?:[^'\\\\]|\\\\.)+')
